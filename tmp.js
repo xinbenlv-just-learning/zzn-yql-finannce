a915e23b7137c98ffba8560db126a378f02a1994
Zainan Victor Zhou <xinbenlv@gmail.com>
2014-11-22 15:13:27 -0800

    Get a list of industries

--- tmp.js
+++ tmp.js
@@ -3,9 +3,7 @@
  * Licensed under MIT License.
  */
 var YQL = require('yql');
-var query = 'select * from yahoo.finance.industry where id in (select industry.id from yahoo.finance.sectors)'
-var yql = new YQL(query);
-var writeToFile = function(myData, outputFilename) {
+var writeJSONToFile = function(myData, outputFilename) {
 	var fs = require('fs');
 	fs.writeFile(outputFilename, JSON.stringify(myData, null, 4), function(err) {
 	    if(err) {
@@ -15,20 +13,38 @@ var writeToFile = function(myData, outputFilename) {
 	    }
 	}); 
 }
-yql.exec(function (error, response) {
-    console.log(JSON.stringify(response, null, 4));
-    writeToFile(response, "response.json");
-    var tickers = [];
-    var industries = response.query.results.industry;
-    for (var i in industries) {
-    	var industry = industries[i];
-    	var companies = industry.company;
-    	for (var c in companies) {
-    		var company = companies[c];
-    		var symbol = company.symbol;
-    		tickers.push(symbol);
-    	}
-    }
-    writeToFile(tickers, "ickers.json");
-});
 
+var writeStringToFile = function(myStr, outputFilename) {
+    var fs = require('fs');
+    fs.writeFile(outputFilename, myStr, function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("Str saved to " + outputFilename);
+        }
+    }); 
+}
+
+var queryGetAllTickers = 'select * from yahoo.finance.industry where id in (select industry.id from yahoo.finance.sectors)'
+new YQL(queryGetAllTickers).exec(function (error, response) {
+    writeJSONToFile(response, "response.json");
+    var tickers = [];
+    var tickersCommaSeparated = "";
+    var industries = response.query.results.industry;
+    var industrieList = [];
+    for (var i in industries) {
+        var industry = industries[i];
+        industrieList.push(industry.name);
+        var companies = industry.company;
+        for (var c in companies) {
+            var company = companies[c];
+            var symbol = company.symbol;
+            tickers.push(symbol);
+            tickersCommaSeparated += ("," + symbol);
+        }
+    }
+
+    writeJSONToFile(industrieList, "industrieList.json");
+    writeJSONToFile(tickers, "tickers.json");
+    writeStringToFile(tickersCommaSeparated, "tickersCommaSeparated.txt");
+});
